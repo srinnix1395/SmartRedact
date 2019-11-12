5525f9a71e33bfeac0571ee391a389102e45bb2e
KienNT8523 <kiennt8523@seta-international.vn>
2019-11-12 15:54:50 +0700

    SplashScreen: edit image

--- app/src/main/java/com/example/smartredact/view/editor/image/EditorImageView.kt
+++ app/src/main/java/com/example/smartredact/view/editor/image/EditorImageView.kt
@@ -1,13 +1,10 @@
 package com.example.smartredact.view.editor.image
 
-import android.widget.ImageView
-import androidx.fragment.app.FragmentActivity
+import android.net.Uri
 import com.example.smartredact.view.base.BaseView
 
 interface EditorImageView : BaseView {
 
-    fun getImageViewPhoto() : ImageView
-
-    fun getActivityEditorImage() : FragmentActivity
+    fun setImageViewPhoto(uri: Uri)
 
 }
